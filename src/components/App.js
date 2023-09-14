fe8fd8148c9d8e63ebde726a892646343ee86455
Altenia Gaines <gaines.tina90@gmail.com>
2023-09-13 18:53:10 -0700

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,34 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
-function App() {
-  const [items, setItems] = useState(itemData);
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  function handleDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
+class App extends Component {
+  // const [items, setItems] = useState(itemData);
+  // const [isDarkMode, setIsDarkMode] = useState(false);
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: itemData,
+      isDarkMode: false,
+    };
   }
+  render() {
+    const handleDarkModeClick = () => {
+      // console.log("mode button pushed");
+      this.setState((state) => ({ isDarkMode: !this.state.isDarkMode }));
+    };
 
-  return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={handleDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
-      <ShoppingList items={items} />
-    </div>
-  );
+    return (
+      <div className={"App " + (this.state.isDarkMode ? "dark" : "light")}>
+        <header>
+          <h2>Shopster</h2>
+          <button onClick={handleDarkModeClick}>
+            {this.state.isDarkMode ? "Dark" : "Light"} Mode
+          </button>
+        </header>
+        <ShoppingList items={this.state.items} />
+      </div>
+    );
+  }
 }
-
 export default App;
